c727ac2da6fd00ef7bed2d0302feb577df2221f3
MOYO <498389182@qq.com>
2019-07-17 13:51:26 +0800

    新建

---
+++ VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by MOYO on 2019/7/17.
+//  Copyright © 2019年 MOYO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
