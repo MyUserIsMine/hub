a142c803651d9afec0c19198b267c491ae4c5194
宋文涛 <498389182@qq.com>
2019-09-02 15:02:13 +0800

    删除多余

---
+++ sdk.framework/Headers/sdk.h
@@ -0,0 +1,21 @@
+//
+//  sdk.h
+//  sdk
+//
+//  Created by yangyuanpeng on 2019/5/2.
+//  Copyright © 2019 yyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdk.
+FOUNDATION_EXPORT double sdkVersionNumber;
+
+//! Project version string for sdk.
+FOUNDATION_EXPORT const unsigned char sdkVersionString[];
+
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk/PublicHeader.h>
+
+
